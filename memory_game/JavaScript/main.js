806e08db6eb2b91c84a733223f6e2a04016e1883
Nawal00 <nawal-grg1993@hotmail.co.uk>
2018-11-11 17:12:01 +0000

    created game logic

--- memory_game/JavaScript/main.js
+++ memory_game/JavaScript/main.js
@@ -1,24 +1,47 @@
-var cards = ["queen", "queen", "king", "king"]; 
+var cards = [
+{
+ rank: "queen",
+ suit: "hearts", 
+ cardImage: "images/queen-of-hearts.png"
+}, 
+{
+ rank: "queen",
+ suit: "diamonds", 
+ cardImage: "images/queen-of-diamonds.png"
+},
+{
+ rank: "king",
+ suit: "hearts", 
+ cardImage: "images/king-of-hearts.png"
+}, 
+{
+ rank: "king",
+ suit: "diamonds", 
+ cardImage:	"images/king-of-diamonds.png"
+}
+]; 
 var cardsInPlay = []; 
 
 //checking for a match if cardsInPlay
 var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
+		console.log("You found a match!");
 	} else {
-	console.log("Sorry, try again.");
+		console.log("Sorry, try again.");
 	}
 }
 
-// function to store stepss when users flips a card
+// logic when users flips a card to add 
 var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId])
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank)
+	console.log("User flipped " + cards[cardId].suit)
+	console.log("User flipped " + cards[cardId].cardImage)
+	cardsInPlay.push(cards[cardId].rank);
 	//logic to check if 2 cads have been played 
-	if(cardsInPlay.length === 2){
+	if (cardsInPlay.length === 2){
 	    checkForMatch();
-		}else {
-			alert("Sorry no match try again!");
+	}else {
+		alert("Sorry no match try again!");
 		}
 }
 
